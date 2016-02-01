1b05f65804a5dce31a30df43e52771aa46768915
Mike Piccolo <mfpiccolo@gmail.com>
2016-01-31 21:52:26 -0800

    Update listings

--- ch_5/listings/5_15_compiler_output_from_listing_5_14_test.rs
+++
@@ -1,8 +0,0 @@
-// src/lib.rs:3:17: 3:30 error: unresolved name `lotto_numbers` [E0425]
-// src/lib.rs:3   assert_eq!(5, lotto_numbers(5).len())
-//                              ^~~~~~~~~~~~~
-//…
-// <std macros>:5:22: 5:33 error: the type of this value must be known in this context
-// <std macros>:5 if ! ( * left_val == * right_val ) {
-//                                     ^~~~~~~~~~~
-//…
--- ch_5/listings/5_19_test_output_from_listing_5_18.rs
+++
@@ -1,4 +0,0 @@
-//failures:
-//
-//---- lotto_numbers_test stdout ----
-//  thread 'lotto_numbers_test' panicked at 'assertion failed: `(left == right)` (left: `5`, right: `0`)', src/lib.rs:7
